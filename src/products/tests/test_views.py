1844e6efaf6f95039e329f40deb74df0ae78dde3
subajat1 <bayumunajat@outlook.com>
2019-03-03 22:19:33 +0800

    implement fixture on views testing

--- src/products/tests/test_views.py
+++ src/products/tests/test_views.py
@@ -5,25 +5,24 @@ from django.contrib.auth.models import User, AnonymousUser
 from products.views import product_detail
 from mixer.backend.django import mixer
 
-@pytest.mark.django_db
-class TestViews(TestCase):
+@pytest.fixture
+def factory():
+    return RequestFactory()
 
-    @classmethod
-    def setUpClass(cls):
-        super(TestViews, cls).setUpClass()
-        mixer.blend('products.Product')
-        cls.factory = RequestFactory()
+@pytest.fixture
+def product(db):
+    return mixer.blend('products.Product')
 
-    def test_product_detail_authenticated(self):
-        path = reverse('detail', kwargs={'pk':1})
-        request = self.factory.get(path)
-        request.user = mixer.blend(User)
-        response = product_detail(request, pk=1)
-        assert response.status_code == 200
+def test_product_detail_authenticated(factory, product):
+    path = reverse('detail', kwargs={'pk':1})
+    request = factory.get(path)
+    request.user = mixer.blend(User)
+    response = product_detail(request, pk=1)
+    assert response.status_code == 200
 
-    def test_product_detail_unauthenticated(self):
-        path = reverse('detail', kwargs={'pk':1})
-        request = self.factory.get(path)
-        request.user = AnonymousUser()
-        response = product_detail(request, pk=1)
-        assert 'accounts/login' in response.url
+def test_product_detail_unauthenticated(factory, product):
+    path = reverse('detail', kwargs={'pk':1})
+    request = factory.get(path)
+    request.user = AnonymousUser()
+    response = product_detail(request, pk=1)
+    assert 'accounts/login' in response.url
