d1e904c54ffbc53d3577e245e0c1c8b8ce84dad4
subajat1 <bayumunajat@outlook.com>
2019-03-03 19:26:08 +0800

    fix pytest using TestCase and cov

--- src/products/tests/test_views.py
+++ src/products/tests/test_views.py
@@ -1,28 +1,29 @@
-from django.test import RequestFactory
+import pytest
+from django.test import TestCase, RequestFactory
 from django.urls import reverse
 from django.contrib.auth.models import User, AnonymousUser
 from products.views import product_detail
 from mixer.backend.django import mixer
 
-import pytest
-
 @pytest.mark.django_db
-class TestViews:
+class TestViews(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestViews, cls).setUpClass()
+        mixer.blend('products.Product')
+        cls.factory = RequestFactory()
 
     def test_product_detail_authenticated(self):
-        mixer.blend('products.Product')
         path = reverse('detail', kwargs={'pk':1})
-        request = RequestFactory().get(path)
+        request = self.factory.get(path)
         request.user = mixer.blend(User)
-
         response = product_detail(request, pk=1)
         assert response.status_code == 200
 
     def test_product_detail_unauthenticated(self):
-        mixer.blend('products.Product')
         path = reverse('detail', kwargs={'pk':1})
-        request = RequestFactory().get(path)
+        request = self.factory.get(path)
         request.user = AnonymousUser()
-
         response = product_detail(request, pk=1)
         assert 'accounts/login' in response.url
